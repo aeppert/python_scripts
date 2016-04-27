0eb69018d96cb7668e6eb7aa31e496a999236415
Aaron Eppert <aeppert@gmail.com>
2016-04-27 08:02:23 -0400

    Update aes_cipher.py

--- aes_cipher.py
+++ aes_cipher.py
@@ -5,22 +5,26 @@ import base64
 from Crypto.Cipher import AES
 from Crypto import Random
 
-BS = 16
-pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS) 
-unpad = lambda s : s[:-ord(s[len(s)-1:])]
-
 class AESCipher:
+    BS = 16
+    
     def __init__( self, key ):
-        self.key = key
-
+        self.key = key[:32]
+        
+    def __pad(self, s): 
+        return s + (AESCipher.BS - len(s) % AESCipher.BS) * chr(AESCipher.BS - len(s) % AESCipher.BS)
+    
+    def __unpad(self, s):
+        return s[:-ord(s[len(s)-1:])]
+    
     def encrypt( self, raw ):
-        raw = pad(raw)
+        raw = self.__pad(raw)
         iv = Random.new().read( AES.block_size )
         cipher = AES.new( self.key, AES.MODE_CBC, iv )
         return base64.b64encode( iv + cipher.encrypt( raw ) ) 
-
+    
     def decrypt( self, enc ):
         enc = base64.b64decode(enc)
         iv = enc[:16]
         cipher = AES.new(self.key, AES.MODE_CBC, iv )
-        return unpad(cipher.decrypt( enc[16:] ))
+        return self.__unpad(cipher.decrypt( enc[16:] ))
